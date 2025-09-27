126234e4eed22e427ae7f1fddafa649dab59c926
Nhan Tic <78673223+trongnhan34vn@users.noreply.github.com>
2025-09-27 12:13:40 +0700

    em Nhan commit

--- src/main/java/com/example/onboarding/presentation/controller/AccountController.java
+++ src/main/java/com/example/onboarding/presentation/controller/AccountController.java
@@ -1,11 +1,8 @@
 package com.example.onboarding.presentation.controller;
 
-import com.example.onboarding.presentation.model.OnboardTransModel;
 import com.example.onboarding.presentation.model.account.AccountRequestBody;
 import com.example.onboarding.presentation.model.account.AccountResponseBody;
 import com.example.onboarding.presentation.service.AccountServicePresentation;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +21,11 @@ public class AccountController {
     @RequestMapping(method = RequestMethod.POST, value = "get-transaction-by-user")
     @ResponseStatus(HttpStatus.OK)
     public AccountResponseBody getListTransactionByUserId(@RequestBody AccountRequestBody accountRequestBody){
+        return accountServicePresentation.getListTransactionByUserId(accountRequestBody);
+    }
+
+    @PostMapping("/test")
+    public AccountResponseBody get(@RequestBody AccountRequestBody accountRequestBody){
         return accountServicePresentation.getListTransactionByUserId(accountRequestBody);
     }
 }
